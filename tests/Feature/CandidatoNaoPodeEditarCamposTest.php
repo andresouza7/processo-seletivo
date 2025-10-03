d0937a1cca9a037a2b252e6dba62d5fcb1355deb
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 <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>
2025-10-03 11:30:55 -0300

    Adiciona novas validações em teste existente (Alterar dados do candidato)

--- tests/Feature/CandidatoNaoPodeEditarCamposTest.php
+++ tests/Feature/CandidatoNaoPodeEditarCamposTest.php
@@ -13,7 +13,7 @@ class CandidatoNaoPodeEditarCamposTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_candidato_nao_pode_editar_campos(): void
+    public function test_candidato_nao_pode_editar_campos_de_identificacao(): void
     {
         // CRIA O USUÁRIO
         $user = InscricaoPessoa::factory()->create([
@@ -41,5 +41,37 @@ class CandidatoNaoPodeEditarCamposTest extends TestCase
             ->assertFormFieldIsDisabled('ci')
             ->assertFormFieldIsDisabled('data_nascimento')
             ->assertFormFieldIsDisabled('email');
+    }
+
+    public function test_candidato_pode_editar_campos_genero_e_contato(): void
+    {
+        // CRIA O USUÁRIO
+        $user = InscricaoPessoa::factory()->create([
+            'nome' => 'tester',
+            'nome_social' => 'nome social tester',
+        ]);
+
+        // LOGA PELO GUARD 'candidato'
+        $this->actingAs($user, 'candidato');
+
+        // ACESSA A PÁGINA
+        $response = $this->get(route('filament.candidato.pages.meus-dados'));
+
+        // VERIFICA SE A PÁGINA CARREGOU
+        $response->assertStatus(200);
+
+        // VERIFICA SE OS CAMPOS ESTÃO HABILITADOS
+        Livewire::test(MeusDados::class)
+            ->assertFormFieldIsEnabled('identidade_genero')
+            ->assertFormFieldIsEnabled('nome_social')
+            ->assertFormFieldIsEnabled('cep')
+            ->assertFormFieldIsEnabled('endereco')
+            ->assertFormFieldIsEnabled('numero')
+            ->assertFormFieldIsEnabled('complemento')
+            ->assertFormFieldIsEnabled('bairro')
+            ->assertFormFieldIsEnabled('cidade')
+            ->assertFormFieldIsEnabled('bairro')
+            ->assertFormFieldIsEnabled('telefone')
+            ;
     }
 }
